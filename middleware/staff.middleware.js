581aa4e62db081f956e46425131f830cd538d98c
Ali Hamza <alihamza@meezansoftwarehouse.com>
2025-07-08 21:04:30 +0500

    set token issue

--- middleware/staff.middleware.js
+++ middleware/staff.middleware.js
@@ -24,11 +24,6 @@ const staffAuth = async (req, res, next) => {
       return res.status(401).json({ message: "Access denied. Staff not found." });
     }
 
-    
-    const permissionNames = (staff.staffRoleId?.permissions || []).map(
-      (perm) => perm.name.toLowerCase()
-    );
-
     // Attach user data to request
     req.user = {
       id: staff._id,
@@ -39,7 +34,7 @@ const staffAuth = async (req, res, next) => {
     req.token = token;
     next();
   } catch (err) {
-    console.error("staffAuth error:", err);
+    
     return res.status(401).json({ message: "Invalid or expired token." });
   }
 };
