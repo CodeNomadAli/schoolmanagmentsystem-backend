0f796fff7dfd09866265bc2195e6a47cdfe98aec
Ali Hamza <alihamza@meezansoftwarehouse.com>
2025-07-15 13:05:23 +0500

    setting email

--- config/mailer.config.js
+++ config/mailer.config.js
@@ -2,12 +2,14 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config()
-const transporter = nodemailer.createTransport({
-  service: 'Gmail', // Or use SMTP settings
+
+var transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    user: "885c3fb24abe4c",
+    pass: "e2bd1df3e39a24"
+  }
 });
 
 export default transporter;
