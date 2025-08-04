fb16560c05082ee65744e4bd073b49a75618917e
Ali Hamza <alihamza@meezansoftwarehouse.com>
2025-08-04 21:32:06 +0500

    fetch data by cate name and ailment

--- routes/for-review-remedy.route.js
+++ routes/for-review-remedy.route.js
@@ -1,9 +1,9 @@
-import { getAllRemedies } from "../controllers/for-review-remedy.controller.js";
+import { getAllRemedies,getAllCategoryAilments } from "../controllers/for-review-remedy.controller.js";
 import express from  "express"
 
 const reviewRemedy= express.Router()
 
-
+reviewRemedy.get("/ailments-categories", getAllCategoryAilments); // ✅ combined
 reviewRemedy.get("/",getAllRemedies)
 
 export default reviewRemedy
