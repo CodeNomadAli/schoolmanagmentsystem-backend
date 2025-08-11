9f74b24b66ca62751f63d33c5c64a2e504495c16
Ali Hyder <cw.alihyder@gmail.com>
2025-08-11 16:57:37 +0500

    cloudinary config

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -5,7 +5,7 @@ import { config } from "dotenv";
 config();
 
 cloudinary.config({
-  CLOUDINARYname: process.env.CLOUDINARY_CLOUDINARYNAME,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
